ee5e970d6841da11308326b5ba85146f3fbd1741
Lera <leravaleriia200518@gmail.com>
2026-02-26 22:00:17 +0100

    Update portfoliolera.js

--- portfoliolera.js
+++ portfoliolera.js
@@ -17,19 +17,20 @@ elements.forEach(el => observer.observe(el));
   emailjs.init("oayX8Ffl7jzCd6ff7");
 })();
 
-const status = document.getElementById("form-status");
+const status = document.getElementById("contact-form");
 
-//document.getElementById("contact-form").addEventListener("submit", function(e)) {
- // e.preventDefault();
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-  emailjs.sendForm("service_3zxcbfe","template_h76paub", this)
-    .then(() => {
-    status.textContent = "Mensaje enviado correctamente.";
-    status.style.color = "#22c55e";
-    form.reset();
-})
-.catch((error) => {
-    status.textContent = "Error al enviar el mensaje.";
-    status.style.color = "#ef4444";
-    console.log(error);
+    emailjs.sendForm(
+        "service_3zxcbfe",
+        "template_h76paub",
+        this
+    ).then(function() {
+        alert("Mensaje enviado correctamente");
+        form.reset();
+    }, function(error) {
+        alert("Error al enviar el mensaje");
+        console.log(error);
+    });
 });
